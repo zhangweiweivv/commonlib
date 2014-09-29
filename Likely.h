2e359ab1cef0fcaa6bf66528bfb58a67a40a7f79
zhangweiweivv <zhangweiweivv@gmail.com>
2014-09-29 23:12:21 +0800

    add Likely.h

---
+++ Likely.h
@@ -0,0 +1,16 @@
+#ifndef LIKELY_H_
+#define LIKELY_H_
+
+#undef LIKELY
+#undef UNLIKELY
+
+#if defined(__GNUC__) && __GNUC__ >= 4
+#define LIKELY(x)   (__builtin_expect((x), 1))
+#define UNLIKELY(x) (__builtin_expect((x), 0))
+#else
+#define LIKELY(x)   (x)
+#define UNLIKELY(x) (x)
+#endif
+
+#endif /* LIKELY_H_ */
+
